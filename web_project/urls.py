04c16abb6870b7d9f73eeff9fc0b06dca5b614c4
WonTaeKim <rladnjsxo123@naver.com>
2019-12-02 13:18:40 +0900

    chore : Add git ignore

--- web_project/urls.py
+++ web_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("hello.urls")),
+    path('polls/', include('polls.urls')),
+    path('',include("blog.urls")),
 ]
